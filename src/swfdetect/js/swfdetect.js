a14333bb1457f1b727b8d3c8f648cf6b6b01d719
Jonathan McCann <jonathan.mccann@liferay.com>
2015-07-23 09:01:42 -0700

    AUI-1945 Change allowScriptAccess to be 'sameDomain' instead of 'always'

--- src/swfdetect/js/swfdetect.js
+++ src/swfdetect/js/swfdetect.js
@@ -41,7 +41,7 @@ else if(uA.ie) {
     try
     {
         ax6 = new ActiveXObject(sF + "." + sF + ".6");
-        ax6.AllowScriptAccess = "always";
+        ax6.AllowScriptAccess = "sameDomain";
     }
     catch (e)
     {
